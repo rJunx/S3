069ff183ed92724c55809ad972d328ddef8a029e
Rongjun Xie <S3525356@rmit.internal>
2016-04-17 23:49:12 +1000

    remove table promotion

--- DB.sql
+++ DB.sql
@@ -1,8 +1,6 @@
 drop table S3T_Customer;
 drop table S3T_Staff;
 drop table S3T_Product;
-drop table S3T_ProductStore;
-drop table S3T_ProductDiscount;
 
 create table S3T_Customer (
   id char(11),
@@ -17,15 +15,6 @@ create table S3T_Staff (
   PRIMARY KEY (id)
 );
 
-create table S3T_Promotion (
-  type int,
-  
-  count int default 0 not null,
-  discount int default 0 not null,
-  
-  PRIMARY KEY (type)
-);
-
 create table S3T_Product (
   barcode varchar(10),
   name varchar(20),
@@ -36,14 +25,9 @@ create table S3T_Product (
   stockLv int default 0 not null,
   replenishLv int default 0 not null,
     
-  PRIMARY KEY (barcode),
-  FOREIGN KEY (promotion) REFERENCES S3T_Promotion(type)
+  PRIMARY KEY (barcode)
 );
 
 INSERT INTO S3T_Customer values ('c0000000001', 0, 0);
 INSERT INTO S3T_Staff values ('s0000000001', 0);
-INSERT INTO S3T_Product values('0000000000', 'Product_0', 1.99);
-INSERT INTO S3T_ProductStore values('0000000000', 10, 2);
-
-INSERT INTO S3T_Promotion values(0);
-INSERT INTO S3T_ProductDiscount values('0000000000');
+INSERT INTO S3T_Product values('0000000000', 'Product_0', 1.99, 0, 0, 0, 0);
